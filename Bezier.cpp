be9e4b295533817fcc2da5b6e0fd23c0415b3c0e
oadziel <josh@jlewisgames.co.uk>
2020-03-16 01:01:18 +0000

    Fixed random stray line issues by implementation of a dedicated operation for the formula - Task 1 Completed

--- Bezier.cpp
+++ Bezier.cpp
@@ -1,43 +1,47 @@
 #include "stdafx.h"
 #include "Bezier.h"
 
+Vector2 Bezier::BezierCalc(float t)const
+{
+	//New Calculated Bezier parameter
+	Vector2 paramB = Vector2();
+	//Set to new point at every iteration
+	Vector2 oldParamB = p0;
+
+	float reducT = (1.0f - t); //reduction parameter
+	float reducTPow = pow(reducT,2); //reduction param squared
+	float reducTPow3 = pow(reducT, 3); //reduction param cubed
+	float tPow = pow(t,2); //t squared
+	float tPow3 = pow(t,3); //t cubed
+
+	
+	paramB.x = reducTPow3 * p0.x + 3 * reducTPow * t * p1.x + 3 * reducT * tPow * p2.x + tPow3 * p3.x; //Bezier Formula for x
+	paramB.y = reducTPow3 * p0.y + 3 * reducTPow * t * p1.y + 3 * reducT * tPow * p2.y + tPow3 * p3.y; //Bezier Formula for y
+
+	//Iterate one forward, next iteration starts with the end of the last
+	oldParamB = paramB;
+	
+	return paramB;
+}
+
 Bezier::Bezier(const Vector2& p0, const Vector2& p1, const Vector2& p2, const Vector2& p3)
 	: p0(p0), p1(p1), p2(p2), p3(p3)
 {
+	//iterate through and calculate bezier by 'BezierCalc'
+	for (size_t i = 0; i < bezPrecision + 1; i++)
+	{
+		float curveParam = float(i) / 20.0f;
+		bezSegments[i] = BezierCalc(curveParam);
+	}
 }
+
 
 void Bezier::draw(SDL_Renderer *renderer) const
 {
-	// TODO
-	//SDL_RenderDrawLineF(renderer, p0.x, p0.y, p1.x, p1.y);
-	//SDL_RenderDrawLine(renderer, p2.x, p2.y, p3.x, p3.y);
-	/*Vector2* points = new Vector2[100];
-	for (int i = 0; i < 1; i++)
+	//Iterate through and render each line
+	for (size_t i = 0; i < bezPrecision; i++)
 	{
-		points[i].x == ((i + 1) / (i / 100));
-	}*/
-	/*SDL_RenderDrawPoint(renderer, p0.x, p0.y);
-	SDL_RenderDrawPoint(renderer, p1.x, p1.y);
-	SDL_RenderDrawPoint(renderer, p2.x, p2.y);
-	SDL_RenderDrawPoint(renderer, p3.x, p3.y);*/
-
-
-	Vector2* points = new Vector2[20];
-	
-	for (float i = 0; i < 1; i += 0.2)
-	{
-		int point = i * 5;
-		points[point] = (pow(1 - i, 3)*p0) + (3 * pow(1 - i, 2)*(i*p1)) + (3 * (1 - i)*pow(i, 2)*p2) + pow(i, 3)*p3;
-	}
-
-	for (int p = 0; p < 100; p++)
-	{
-		int nP = p + 1;
-		if (nP > 20)
-		{
-			nP = 0;
-		}
-		SDL_RenderDrawLineF(renderer, points[p].x, points[p].y, points[nP].x, points[nP].y);
+		SDL_RenderDrawLineF(renderer, bezSegments[i].x, bezSegments[i].y, bezSegments[i+1].x, bezSegments[i+1].y);
 	}
 	
 }
--- Bezier.h
+++ Bezier.h
@@ -8,7 +8,13 @@ class Bezier
 public:
 	Bezier(const Vector2& p0, const Vector2& p1, const Vector2& p2, const Vector2& p3);
 
+	Vector2 BezierCalc(float t)const;
+
 	void draw(SDL_Renderer *renderer) const;
+
+	const int static bezPrecision = 20;
+	Vector2 bezSegments[bezPrecision + 1];
+	Vector2 bezTangent[bezPrecision + 1];
 
 private:
 	Vector2 p0, p1, p2, p3;
